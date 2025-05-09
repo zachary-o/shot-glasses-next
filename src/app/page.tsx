80ee098e9ad12ee6da1fff55b32b88d5591e2e7c
Zakhar Osetskyi <oset.zak@bmbsoft.com.vn>
2025-05-09 17:15:25 +0700

    install fonts and languages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
-import {useSet} from 'react-use';
+"use client"
 
-
+import { Locale } from "@/i18n/config"
+import { useTranslations } from "next-intl"
+import { useTransition } from "react"
+import { setUserLocale } from "./services/locale"
+import clsx from "clsx"
 
 export default function Home() {
+  const t = useTranslations("HomePage")
+  const [isPending, startTransition] = useTransition()
+
+  function onClick(value: string) {
+    const locale = value as Locale
+    startTransition(() => {
+      setUserLocale(locale)
+    })
+  }
   return (
-    <main></main>
-  );
+    <main className="font-normal space-y-4">
+      <div className="space-x-2">
+        <button
+          className={clsx(
+            "rounded-sm p-2 transition-colors hover:bg-slate-200 cursor-pointer",
+            isPending && "pointer-events-none opacity-60"
+          )}
+          onClick={() => onClick("en")}
+        >
+          EN
+        </button>
+        <button
+          className={clsx(
+            "rounded-sm p-2 transition-colors hover:bg-slate-200 cursor-pointer",
+            isPending && "pointer-events-none opacity-60"
+          )}
+          onClick={() => onClick("uk")}
+        >
+          UKR
+        </button>
+      </div>
+      <h1>{t("title")}</h1>
+    </main>
+  )
 }
---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Header = () => {
+  return <header className="h-18 flex items-center">Header</header>
+}
+
+export default Header
