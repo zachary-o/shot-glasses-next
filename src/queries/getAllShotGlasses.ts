f49bc50f7eb791b615adbfaa84f4e83523ccde94
Zach <104291766+zachary-o@users.noreply.github.com>
2025-06-27 18:33:46 +0700

    fix deployment issue

--- src/queries/getAllShotGlasses.ts
+++ src/queries/getAllShotGlasses.ts
@@ -1,27 +1,27 @@
-import { prisma } from "@/prisma"
-import { GetSearchParams } from "@/types"
-import { Prisma, ShotGlass } from "@prisma/client"
+import { prisma } from "@/prisma";
+import { GetSearchParams } from "@/types";
+import { Prisma, ShotGlass } from "@prisma/client";
 
 export const getAllShotGlasses = async (
   searchParams: GetSearchParams,
   locale: string
 ): Promise<ShotGlass[]> => {
-  const continents = searchParams.continents?.split(",") || []
-  const countries = searchParams.countries?.split(",") || []
-  const search = searchParams.search?.trim() || ""
-  const sortBy = searchParams.sortBy?.trim() || ""
+  const continents = searchParams.continents?.split(",") || [];
+  const countries = searchParams.countries?.split(",") || [];
+  const search = searchParams.search?.trim() || "";
+  const sortBy = searchParams.sortBy?.trim() || "";
 
-  const isLocaleEng = locale === "en"
+  const isLocaleEng = locale === "en";
 
-  let where: Prisma.ShotGlassWhereInput = {}
-  let orderBy: Prisma.ShotGlassOrderByWithRelationInput = {}
+  const where: Prisma.ShotGlassWhereInput = {};
+  let orderBy: Prisma.ShotGlassOrderByWithRelationInput = {};
 
   if (continents && continents.length > 0) {
-    where.continentEng = { in: continents }
+    where.continentEng = { in: continents };
   }
 
   if (countries && countries.length > 0) {
-    where.countryEng = { in: countries }
+    where.countryEng = { in: countries };
   }
 
   if (search && search.length > 0) {
@@ -35,23 +35,23 @@ export const getAllShotGlasses = async (
           { cityUkr: { contains: search, mode: "insensitive" } },
           { countryUkr: { contains: search, mode: "insensitive" } },
           { continentUkr: { contains: search, mode: "insensitive" } },
-        ]
+        ];
   }
 
   if (sortBy && sortBy.length > 0) {
     switch (sortBy) {
       case "cityAsc":
-        orderBy = { [isLocaleEng ? "cityEng" : "cityUkr"]: "asc" }
-        break
+        orderBy = { [isLocaleEng ? "cityEng" : "cityUkr"]: "asc" };
+        break;
       case "cityDesc":
-        orderBy = { [isLocaleEng ? "cityEng" : "cityUkr"]: "desc" }
-        break
+        orderBy = { [isLocaleEng ? "cityEng" : "cityUkr"]: "desc" };
+        break;
       case "countryAsc":
-        orderBy = { [isLocaleEng ? "countryEng" : "countryUkr"]: "asc" }
-        break
+        orderBy = { [isLocaleEng ? "countryEng" : "countryUkr"]: "asc" };
+        break;
       case "countryDesc":
-        orderBy = { [isLocaleEng ? "countryEng" : "countryUkr"]: "desc" }
-        break
+        orderBy = { [isLocaleEng ? "countryEng" : "countryUkr"]: "desc" };
+        break;
       // case "popularityAsc":
       //   orderBy = { likes: "asc" }
       //   break
@@ -59,7 +59,7 @@ export const getAllShotGlasses = async (
       //   orderBy = { likes: "desc" }
       //   break
       default:
-        break
+        break;
     }
   }
 
@@ -67,8 +67,8 @@ export const getAllShotGlasses = async (
     return await prisma.shotGlass.findMany({
       where,
       orderBy,
-    })
+    });
   } catch (error) {
-    throw new Error(`Failed to fetch shot glasses: ${error}`)
+    throw new Error(`Failed to fetch shot glasses: ${error}`);
   }
-}
+};
